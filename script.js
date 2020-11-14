a38f8ab2a02c61d69590630ef4b4d52b407520e3
Raphael Buriti <30700164+raphaelburiti@users.noreply.github.com>
2020-11-14 13:51:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-    function reqListener() {
-        var html = this.responseText;
-        var cpf = html.split('<dt>CPF</dt>')[1].split('<dd>')[1].split('</dd>')[0];
-        console.log(cpf);
-    };
+function reqListener() {
+    var html = this.responseText;
+    var cpf = html.split('<dt>CPF</dt>')[1].split('<dd>')[1].split('</dd>')[0];
+    console.log(cpf);
+    insertCpfInputForm(cpf);
+};
 
-    var oReq = new XMLHttpRequest();
-    oReq.onload = reqListener;
-    oReq.open('get', 'https://universidade.bioslogus.com.br/user/profile.php?id=2');
-    oReq.send();
+var oReq = new XMLHttpRequest();
+oReq.onload = reqListener;
+oReq.open('get', 'https://universidade.bioslogus.com.br/user/profile.php?id=2');
+oReq.send();
+
+function insertCpfInputForm(cpf) {
+    const inputFomr = document.querySelector('.quantumWizTextinputPaperinputInputArea input');
+    inputFomr.value = cpf;
+}
